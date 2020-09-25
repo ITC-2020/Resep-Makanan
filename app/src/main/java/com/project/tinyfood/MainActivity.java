c1015d9b50fbd21f6ac83d2dab536ac0b461f0df
Liek Allyandaru <all4yandaru@gmail.com>
2020-09-25 17:53:20 +0700

    Fragment

--- app/src/main/java/com/project/tinyfood/MainActivity.java
+++ app/src/main/java/com/project/tinyfood/MainActivity.java
@@ -1,14 +1,52 @@
 package com.project.tinyfood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.btm_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.menu_home:
+                loadFragment(new HomeFragment());
+                break;
+
+            case R.id.menu_favorite:
+                loadFragment(new FavoritesFragment());
+                break;
+
+            case R.id.menu_recipes:
+                loadFragment(new RecipesFragment());
+                break;
+        }
+        return true;
+    }
+
+    private void loadFragment(Fragment selected){
+        if(selected != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame,selected)
+                    .show(selected)
+                    .commit();
+        }
     }
 }
